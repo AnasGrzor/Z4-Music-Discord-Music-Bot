230f7ff1b765667b52e4a0aeaef6f70b8246b60d
anasgrzor <anaskhalid658@gmail.com>
2024-04-20 19:20:14 +0500

    Added License.md

--- mainbot.py
+++ mainbot.py
@@ -1,25 +1,12 @@
 """
-MIT License
+Author: AnasGrzor
 
-Copyright (c) 2019-Current PythonistaGuild, EvieePy
+Implemented Using Wavelink Library (https://github.com/PythonistaGuild/Wavelink)
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+Description:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+This Discord bot is your ultimate companion for music playback in your server. With a wide range of commands, it enables seamless control over the music playing experience. From playing songs using keywords or URLs to toggling playback filters like nightcore and slowed, managing the queue, adjusting volume, and seeking to specific positions within tracks, this bot provides a comprehensive suite of features to enhance your Discord music sessions. Whether you're hosting a listening party, chilling with friends, or simply enjoying your favorite tunes, this bot ensures a smooth and customizable music playback experience tailored to your preferences.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
 """
 import asyncio
 import logging
